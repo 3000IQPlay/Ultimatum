73ea5b304be591df7b8bbcccd17e686ec35f836e
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2023-10-29 00:06:21 +0200

    VPN Smokery

--- auth/VPNDetector.java
+++ auth/VPNDetector.java
@@ -1,60 +1,33 @@
 package auth;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.net.URL;
-
-import net.vpnblocker.api.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 public class VPNDetector {
-    
-    private String getPublicIP() {
-        String ip = "";
+    public static boolean isCloudFlareVPN() {
         try {
-            URL url = new URL("https://checkip.amazonaws.com");
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            ip = br.readLine().trim();
-        } catch (Exception e) {
-            ip = "Error: " + e.getMessage();
-        }
-        return ip;
-    }
-
-    public Boolean isVPN() {
-        String ipToLookup = getPublicIP();
-        try {
-            Boolean isHostingorVPN = new VPNDetection().getResponse(ipToLookup).hostip;
-            return isHostingorVPN;
-        } catch (IOException e) {
-            // System.out.println("Error: " + e.getMessage());
-            return null;
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String hostName = inetAddress.getHostName();
+            return hostName.contains("CloudFlare Inc.");
+        } catch (UnknownHostException e) {
+            return false;
         }
     }
+	
+	public static boolean isAvastVPN() throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 
-    public void getIPInfo() {
-        String ipToLookup = getPublicIP();
-        new Thread(() -> {
-            try {
-                VPNDetection vpn_detection = new VPNDetection();
-                Response api_response = vpn_detection.getResponse(ipToLookup);
-                if (api_response.status.equals("success")) {
-                    // System.out.println("Package: " + api_response.getPackage);
-                    if (api_response.getPackage.equals("Free")) {
-                        // System.out.println("Remaining Requests: " + api_response.remaining_requests);
-                    }
-                    // System.out.println("IP Address: " + api_response.ipaddress);
-                    // System.out.println("Is this IP a VPN or Hosting Network? " + api_response.hostip);
-                    // System.out.println("Organisation: " + api_response.org);
-                    if (api_response.country != null) {
-                        // System.out.println("Country: " + api_response.country.name);
-                    }
-                } else {
-                    // System.out.println("Error: " + api_response.msg);
-                }
-            } catch (IOException ex) {
-                // System.out.println("Error: " + ex.getMessage());
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = interfaces.nextElement();
+            String interfaceName = networkInterface.getDisplayName();
+
+            if (interfaceName.equals("Avast")) {
+                return true;
             }
-        }).start();
+        }
+		return false;
     }
 }
