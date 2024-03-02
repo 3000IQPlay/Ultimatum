716ad8d1f5fd15b0ceec788504a55aedcd2d0d10
3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2024-03-02 20:57:37 +0100

    Update SelfDestruct.java

--- auth/SelfDestruct.java
+++ auth/SelfDestruct.java
@@ -2,27 +2,27 @@ package auth;
 
 import java.io.File;
 import java.nio.file.Files;
-import lol.fabuls.utilities.system.SystemUtil;
+import java.net.URISyntaxException;
 
-public class SelfDestruct {
+public class SelfDelete {
 
     private static void selfDestructWindowsJARFile() throws Exception {
-        String currentJARFilePath = SelfDestruct.getCurrentJarPath().toString();
+        String currentJARFilePath = SelfDelete.getCurrentJarPath().toString();
         Runtime runtime = Runtime.getRuntime();
         runtime.exec("cmd /c ping localhost -n 2 > nul && del \"" + currentJARFilePath + "\"");
     }
 
     public static void selfDestructJARFile() throws Exception {
-		String os = System.getProperty("os.name").toLowerCase();
+	String os = System.getProperty("os.name").toLowerCase();
         if (os.contains("win")) {
-            SelfDestruct.selfDestructWindowsJARFile();
+            SelfDelete.selfDestructWindowsJARFile();
         } else {
-            File directoryFilePath = SelfDestruct.getCurrentJarPath();
+            File directoryFilePath = SelfDelete.getCurrentJarPath();
             Files.delete(directoryFilePath.toPath());
         }
     }
 	
-	public static File getCurrentJarPath() throws URISyntaxException {
-        return new File(AbstractProtector.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+    public static File getCurrentJarPath() throws URISyntaxException {
+        return new File(SelfDelete.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
     }
 }
