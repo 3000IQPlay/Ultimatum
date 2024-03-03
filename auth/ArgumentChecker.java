19709c983f487b93fae1ed380f9fdcda1c700e4a
3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2024-03-03 11:15:50 +0100

    Update ArgumentChecker.java

--- auth/ArgumentChecker.java
+++ auth/ArgumentChecker.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ArgumentChecker {
 	private static final String[] naughtyFlags = {
-			"-agentlib:jdwp",
+	    "-agentlib:jdwp",
             "-XBootclasspath",
             "-javaagent",
             "-Xdebug",
@@ -23,29 +23,38 @@ public class ArgumentChecker {
     };
 
     public static void checkArgument() {
-        if (isDebuggerOrAgentAttached()) {
-			// Informs the developers about a suspicious activity
-			WebhookInformer.sendFlag();
-			
+	for (String str : naughtyFlags) {
+   		if (!str.contains(String.valueOf("-"))) {
 			// Create a CallSite
 			CallSite callSite = generateExitCallSite();
 
 			// Invoke the "exit()" method using the CallSite
 			callSite.invokeInt(0);
 			
-			/*Class<?> systemClass = Class.forName("java.lang.System");
-			Method method = systemClass.getDeclaredMethod("exit", int.class);
-			method.invoke(null, 0);*/
-            // System.out.println("Debugger or Agent detected! Exiting...");
-			// Continue with the rest of your code
+      			System.out.println("DEV MODE: Array modifications have been detected");
+    		}
+  	}
+        if (isDebuggerOrAgentAttached() || naughtyFlags.length != 12 || ) {
+		// Informs the developers about a suspicious activity
+		WebhookInformer.sendFlag();
+			
+		// Create a CallSite
+		CallSite callSite = generateExitCallSite();
+
+		// Invoke the "exit()" method using the CallSite
+		callSite.invokeInt(0);
+			
+		/*Class<?> systemClass = Class.forName("java.lang.System");
+		Method method = systemClass.getDeclaredMethod("exit", int.class);
+		method.invoke(null, 0);*/
+                System.out.println("DEV MODE: Debugger or Agent detected! Exiting...");
         } else {
-            // System.out.println("No debugger or agent detected.");
-            // Continue with the rest of your code
+             System.out.println("DEV MODE: No debugger or agent detected.");
         }
     }
 
     private static boolean isDebuggerOrAgentAttached() {
-		RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
+	RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
         String jvmArgs = runtimeBean.getInputArguments().toString();
 		
         for (String arg : naughtyFlags) {
@@ -59,7 +68,7 @@ public class ArgumentChecker {
 	
 	// bootstrapExit method to generate the "MethodHandle" for "System.exit()"
     public static CallSite bootstrapExit() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-		Class<?> klass = Class.forName("java.lang.System");
+	Class<?> klass = Class.forName("java.lang.System");
         MethodHandle methodHandle = MethodHandles.lookup().findStaticMethod(klass, "exit", int.class);
         return new ConstantCallSite(methodHandle);
     }
